31de48ee409d752d138eb8576a612263fafcb821
Keshav Kumar <46888254+keshavsharma5324@users.noreply.github.com>
2024-06-25 17:03:14 +0530

    Update typeing.js

--- script/typeing.js
+++ script/typeing.js
@@ -1,7 +1,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Web Designer", "Video Editor", "Blogger", "Content Writer", "Android Developer", "YouTuber"];
+const textArray = ["Flutter Developer", "iOS Developer", "Flutter Web Developer", "Mobile Developer"];
 const typingDelay = 80;
 const erasingDelay = 80;
 const newTextDelay = 100; // Delay between current and next text
